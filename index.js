36f249b9aa274e5cc1b9375cf4128ab90256a1d8
lmdung94 <lmdung94@gmail.com>
2020-01-08 21:45:45 +0700

    update code

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 app.get('/', function(req,res){
 	res.send('Hello world')
 })
+app.get('/users', function(req, res){
+	res.send('user list')
+})
 app.listen(port, function(){
 	console.log('App listening on port ' + port)
 })
