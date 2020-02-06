e161b56105591e9224304249337faaa39365cde2
dung94 <lmdung94@gmail.com>
2020-02-06 13:46:48 +0700

    code validation

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -26,6 +26,20 @@ module.exports.viewByID = (req, res) => {
 };
 module.exports.postCreate = (req, res) => {
     req.body.id = shortid.generate();
+    let errors = [];
+    if(!req.body.name || req.body.name.trim() === '') {
+        errors.push("Name is required")
+    };
+    if(!req.body.phone || req.body.phone.trim() ==='') {
+        errors.push("Phone is required")
+    }
+    if (errors.length) {
+        res.render('users/create',{
+            errors: errors,
+            values: req.body
+        })
+        return;
+    }
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
