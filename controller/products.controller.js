03d4f5c2742b68ec5c7a43e05e7d16d53846cf11
dung94 <lmdung94@gmail.com>
2020-02-08 17:55:59 +0700

    a

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,6 +1,10 @@
 const db = require('../db');
 module.exports.products = (req, res) => {
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 8;
+    var begin = (page -1)*perPage;
+    var end = page*perPage
     res.render('products/products', {
-        products: db.get('products').value(),
+        products: db.get('products').value().slice(begin,end)
     });
 };
