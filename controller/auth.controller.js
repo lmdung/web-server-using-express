f4fdd527d36ddbabb6f19b01a330b7f89c72f1ec
dung94 <lmdung94@gmail.com>
2020-02-12 17:44:14 +0700

    mongose

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,6 @@
 const db = require('../db');
 const md5 = require('md5');
-
+var User = require('../models/user.model');
 module.exports.login = (req, res) => {
     res.render('auth/login');
 };
@@ -8,6 +8,7 @@ module.exports.portLogin = (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
     var user = db.get('users').find({ email: email }).value();
+    // var user = User.find()
     if (!user) {
         res.render('auth/login', {
             errors: [
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,7 @@
-const db = require('../db');
-const shortid = require('shortid');
+// const db = require('../db');
+// const shortid = require('shortid');
 var User = require('../models/user.model');
+
 module.exports.index = (req, res) => {
     // res.render('users/index', {
     //     users: db.get('users').value(),
@@ -12,14 +13,17 @@ module.exports.index = (req, res) => {
     }))
 };
 module.exports.search = (req, res) => {
-    const q = '/'+ req.query.q + '/'
+    const q = req.query.q;
     // const matchedUsers = db.get('users').filter(el => {
     //     return el.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
     // }).value();
     // res.render('users/index', {
     //     users: matchedUsers
     // });
-    User.findOne({"name":q}).then((users => {
+    User.find().then((listUsers => {
+        var users = listUsers.filter(el => {
+            return el.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+        })
         res.render('users/index', {
             users: users
         })
@@ -31,21 +35,24 @@ module.exports.create = (req, res) => {
 };
 module.exports.viewByID = (req, res) => {
     const id = req.params.id;
-    const user = db.get('users').find({ id: id }).value();
-    res.render('users/view', {
-        user: user
-    });
-    // User.find({ id: id }).then((user => {
-    //     res.render('users/view', {
-    //         user: user
-    //     })
-    // }))
+    // const user = db.get('users').find({ id: id }).value();
+    // res.render('users/view', {
+    //     user: user
+    // });
+    User.findById(id, (err, user) => {
+        res.render('users/view', {
+            user: user
+        })
+    })
 };
 module.exports.postCreate = (req, res) => {
-    req.body.id = shortid.generate();
+    // req.body.id = shortid.generate();
     // req.body.avatar = req.file.path.split('/').slice(1).join('/');
-    req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+    // req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+    req.body.avatar = req.file.path.replace('public','');
     // kiem tra lai
-    db.get('users').push(req.body).write();
+    // db.get('users').push(req.body).write();
+    // res.redirect('/users');
+    User.create(req.body)
     res.redirect('/users');
 };
